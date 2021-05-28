c2b5b3cf9954903ffd4e7051c6adf38b9a338cf0
Mike <mandmeier24@gmail.com>
2021-05-27 21:48:48 -0500

    test api to backend

--- src/state/actions/evoActions.js
+++ src/state/actions/evoActions.js
@@ -1,16 +1,13 @@
+import * as api from '../api';
+
+
+
 export const getEvos = () => async dispatch => {
     try {
-        const evos = [
-            {id: "biomorph-00001-1", svg: `<svg width="100%" height="100%" viewBox="0 0 300 300" preserveAspectRatio="xMidYMid" style="background: none;"><g stroke="black" stroke-width="2"><line x1="150" y1="128" x2="166" y2="160"></line>`
-            },
-            {id: "biomorph-00002-1", svg: `<svg width="100%" height="100%" viewBox="0 0 300 300" preserveAspectRatio="xMidYMid" style="background: none;"><g stroke="black" stroke-width="2"><line x1="150" y1="108" x2="150" y2="128"></line><line x1="150" y1="128" x2="166" y2="160"></line>`
-            },
-            {id: "biomorph-00003-1", svg: `<svg width="100%" height="100%" viewBox="0 0 300 300" preserveAspectRatio="xMidYMid" style="background: none;"><g stroke="black" stroke-width="2"><line x1="150" y1="108" x2="150" y2="128"></line><line x1="166" y1="160" x2="166" y2="136"></line><line x1="166" y1="136" x2="150" y2="128"></line>`
-            },
-            {id: "biomorph-00004-2", svg: `<svg width="100%" height="100%" viewBox="0 0 300 300" preserveAspectRatio="xMidYMid" style="background: none;"><g stroke="black" stroke-width="2"><line x1="150" y1="108" x2="150" y2="128"></line><line x1="150" y1="128" x2="166" y2="160"></line><line x1="166" y1="160" x2="166" y2="136"></line><line x1="166" y1="136" x2="150" y2="128"></line><line x1="150" y1="128" x2="150" y2="124"></line><line x1="150" y1="124" x2="150" y2="124"></line></g></svg>`
-            }
-        ]
-        dispatch({type: "GET_EVOS", payload: evos})         
+        const { data } = await api.getEvos();
+        console.log("Fetched Data")
+        console.log(data)
+        dispatch({type: "GET_EVOS", payload: data})         
       } catch (error) {
         console.log(error);
       }
---
+++ src/state/api/index.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+//const url = 'https://darwinstees.herokuapp.com';
+const url = 'http://localhost:5000';
+
+export const getEvos = () => axios.get(`${url}/server/evos`);
