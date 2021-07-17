fda27eedd2fc734108272e2db15528bb6b9ba9a1
Mike <mandmeier24@gmail.com>
2021-07-17 17:28:57 -0500

    Heroku deployment prep

--- server.js
+++ server.js
@@ -23,10 +23,14 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
+} else {
+    app.get('/', (req, res) => {
+        res.send("Api running")
+    });
 }
 
 mongoose
